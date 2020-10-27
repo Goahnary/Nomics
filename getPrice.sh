1378ff338942180d444eadb8b0e1c6496ed71994
Noah Gary <goahnary@gmail.com>
2020-10-27 18:49:50 -0400

    updating curl request. WIP transformation in bash.

--- getPrice.sh
+++ getPrice.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
-echo "curl -XGET https://api.nomics.com/v1/currencies/ticker"
-curl -s -o result.json -XGET "https://api.nomics.com/v1/currencies/ticker?key=${NOMIC_API_KEY}&ids=BTC,ETH,XRP&interval=1d,30d&per-page=100&page=1"
+echo "curl -XGET https://api.nomics.com/v1/currencies/sparkline"
+curl -s -o result.json -XGET "https://api.nomics.com/v1/currencies/sparkline?key=${NOMIC_API_KEY}&ids=BTC&start=2020-01-01T00%3A00%3A00Z&end=2020-10-26T00%3A00%3A00Z"
 
+'''
+timestamps=(`cat result.json | jq '.[].timestamps[]'`)
+prices=(`cat result.json | jq '.[].prices[]'`)
+
+echo ${!timestamps[*]}
+: > export.json
+echo "[" >> export.json
+
+switched=false
+for i in ${!timestamps[*]}; do
+	if [ !$switched ]; then
+		switched=true
+		echo "switching!"
+	else
+		echo "," >> export.json
+	fi
+	echo "{\"timestamp\": ${timestamps[$i]},\"price\": ${prices[$i]}}" >> export.json
+done
+
+echo "]" >> export.json
+'''
