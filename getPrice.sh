792fc3bc9fbded0e93f627138c608966fa3086b2
Noah Gary <goahnary@gmail.com>
2020-10-28 01:23:53 -0400

    Getting dynamic start/end dates so this can be run any year and still be valid

--- getPrice.sh
+++ getPrice.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
+startDate="$(date +'%Y')-01-01T00%3A00%3A00Z"
+stopDate="$(date +'%Y-%m-%d')T00%3A00%3A00Z"
 echo "curl -XGET https://api.nomics.com/v1/currencies/sparkline"
-curl -s -o result.json -XGET "https://api.nomics.com/v1/currencies/sparkline?key=${NOMIC_API_KEY}&ids=BTC&start=2020-01-01T00%3A00%3A00Z&end=2020-10-26T00%3A00%3A00Z"
+curl -s -o result.json -XGET "https://api.nomics.com/v1/currencies/sparkline?key=${NOMIC_API_KEY}&ids=BTC&start=${startDate}&end=${stopDate}"
 
-#timestamps=(`cat result.json | jq '.[].timestamps[]'`)
-#prices=(`cat result.json | jq '.[].prices[]'`)
-
-#echo ${!timestamps[*]}
-#: > export.json
-#echo "[" >> export.json
-
-#switched=false
-#for i in ${!timestamps[*]}; do
-#	if [ !$switched ]; then
-#		switched=true
-#		echo "switching!"
-#	else
-#		echo "," >> export.json
-#	fi
-#	echo "{\"timestamp\": ${timestamps[$i]},\"price\": ${prices[$i]}}" >> export.json
-#done
-
-#echo "]" >> export.json
